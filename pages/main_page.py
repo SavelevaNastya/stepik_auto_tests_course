1fdd989076596b3bf0392090dad39de0537bbb90
SavelevaNastya <71922046+SavelevaNastya@users.noreply.github.com>
2022-04-02 23:09:46 +0700

    add assert in go_to_login_page-method

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,8 +3,9 @@ from selenium.webdriver.common.by import By
 
 class MainPage(BasePage): # наследник класса BasePage. Класс-предок в Python указывается в скобках
     def go_to_login_page(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click() 
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
